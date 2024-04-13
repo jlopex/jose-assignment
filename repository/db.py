814632c1019b33be5ae3caa0c62919eac5bfb2f3
Jose Rodriguez <boriel@gmail.com>
2024-04-13 19:36:08 +0200

    Add simple DB interface
    This will create a local SQlite file and populate
    new tables if they don´t exist yet.

---
+++ repository/db.py
@@ -0,0 +1,23 @@
+# For simplicity's sake, we don´t do migrations
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import config
+from .model._base import Base  # noqa
+
+
+db = create_engine(config.DB_NAME, connect_args={"check_same_thread": False})
+db.echo = config.DB_ECHO
+Base.metadata.create_all(db)  # to be replaced by alembic migrations
+Session = sessionmaker(bind=db, autoflush=False, autocommit=False)
+
+
+@contextmanager
+def new_session():
+    session = Session()
+    try:
+        yield session
+    finally:
+        session.close()
