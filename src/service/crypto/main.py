c9b962664cb7dda2f0ca6443cd3c76203d69f70c
Jose Rodriguez <boriel@gmail.com>
2024-04-14 22:01:26 +0200

    refact: allow Content and ContentCreate in CryptoService
    This way we can create an encrypted Content before storing
    it in the db.

--- src/service/crypto/main.py
+++ src/service/crypto/main.py
@@ -1,9 +1,15 @@
 # Implements a dummy crypto service
+from typing import TypeVar
+
 from src import config
-from src.domain.content import Content
+from src.domain.content import Content, ContentCreate
 from .drivers.aes_cbc import AesCbcDriver
 from .drivers.aes_ecb import AesEcbDriver
 from .drivers.base import BaseCryptoDriver
+from ...domain.protection_system import ProtectionSystem
+from ...repository.protection_system import ProtectionSystemRepository
+
+_T = TypeVar("_T", Content, ContentCreate)
 
 
 class CryptoService:
@@ -32,35 +38,57 @@ class CryptoService:
         return cls.get_driver(driver).decrypt(content, key)
 
     @classmethod
-    def encrypt(cls, content: Content) -> Content:
-        if content.is_encrypted:
-            return content
+    def _get_protection_system(cls, content: _T) -> ProtectionSystem:
+        if isinstance(content, Content):
+            return content.protection_system
+
+        return ProtectionSystemRepository.get(content.protection_system_id)
+
+    @classmethod
+    def _assemble_content(
+        cls, content: _T, encrypted_payload: bytes, *, is_encrypted: bool
+    ) -> _T:
+        attrs = content.model_dump(exclude={"encrypted_payload", "is_encrypted"})
+
+        if isinstance(content, ContentCreate):
+            return ContentCreate(
+                **attrs, is_encrypted=is_encrypted, encrypted_payload=encrypted_payload
+            )
 
         return Content(
-            id=content.id,
-            encrypted_payload=cls._encrypt(
-                content.protection_system.encryption_mode,
-                content.encrypted_payload,
-                content.encryption_key,
-            ),
-            is_encrypted=True,
-            encryption_key=content.encryption_key,
-            protection_system=content.protection_system,
+            **attrs,
+            is_encrypted=is_encrypted,
+            encrypted_payload=encrypted_payload,
         )
 
     @classmethod
-    def decrypt(cls, content: Content) -> Content:
+    def encrypt(cls, content: _T) -> _T:
+        if content.is_encrypted:
+            return content
+
+        protection_system = cls._get_protection_system(content)
+        encrypted_payload = cls._encrypt(
+            protection_system.encryption_mode,
+            content.encrypted_payload,
+            content.encryption_key,
+        )
+
+        return cls._assemble_content(
+            content=content, encrypted_payload=encrypted_payload, is_encrypted=True
+        )
+
+    @classmethod
+    def decrypt(cls, content: _T) -> _T:
         if not content.is_encrypted:
             return content
 
-        return Content(
-            id=content.id,
-            encrypted_payload=cls._decrypt(
-                content.protection_system.encryption_mode,
-                content.encrypted_payload,
-                content.encryption_key,
-            ),
-            is_encrypted=False,
-            encryption_key=content.encryption_key,
-            protection_system=content.protection_system,
+        protection_system = cls._get_protection_system(content)
+        encrypted_payload = cls._decrypt(
+            protection_system.encryption_mode,
+            content.encrypted_payload,
+            content.encryption_key,
+        )
+
+        return cls._assemble_content(
+            content=content, encrypted_payload=encrypted_payload, is_encrypted=False
         )
